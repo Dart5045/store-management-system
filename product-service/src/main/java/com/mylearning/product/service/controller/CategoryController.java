1dbd90733da031a5049d42d7281a1c7d17a24e73
Bladimir Gonzales <dart5045@gmail.com>
2023-09-24 18:47:04 +0200

    Add Spring cloud config server

---
+++ product-service/src/main/java/com/mylearning/product/service/controller/CategoryController.java
@@ -0,0 +1,26 @@
+package com.mylearning.product.service.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/category")
+@RefreshScope
+public class CategoryController {
+    @Value("${app.testProp}")
+    private String testProp;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public String getTestProp()
+    {
+        System.out.println("blass");
+        return testProp;
+    }
+}
